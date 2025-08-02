1650bffde8250f9cd18f617b163b350beb908930
NewPlayer99 <2022977537@isiswa.uitm.edu.my>
2025-08-02 16:05:20 +0800

    Trained model with new dataset

--- app/routers/fraud.py
+++ app/routers/fraud.py
@@ -1,11 +1,14 @@
 import joblib
 from fastapi import APIRouter
 from ..models.fraud import FraudInput
+import os
+from pathlib import Path
+
 
 router = APIRouter()
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(base_dir, "models", "fraud_model_v2.pkl")
+
+model_path = Path(__file__).resolve().parent.parent / "models" / "fraud_model_v2.pkl"
 model = joblib.load(model_path)
 
 @router.post("/fraud/predict")
